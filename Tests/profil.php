8866c3886c0e106002e2e31239bfc1a04cd6f126
Xeliok <130991034+Xeliok@users.noreply.github.com>
2024-05-23 18:29:12 +0200

    Add files via upload

--- Tests/profil.php
+++ Tests/profil.php
@@ -9,6 +9,30 @@
     if(isset($_POST['recherche'])){
         header("Location:search.php");
     }
+
+    if(isset($_POST['video-url-input'])){
+        $video_url = $_POST['video-url-input'];
+        $email = $_SESSION['mail'];
+
+        $json_file = file_get_contents("Comptes/$email.json");
+        $json_data = json_decode($json_file, true);
+        $json_data['ytb_video'] = $video_url;
+        file_put_contents("Comptes/$email.json", json_encode($json_data));
+
+        $json_file = file_get_contents("Comptes/users.json");
+        $users_file = file_get_contents("Comptes/users.json");
+        $users_data = json_decode($users_file, true);
+        foreach ($users_data as $key => $user) {
+            if ($user['mail'] === $email) {
+                // Update the video URL for the user
+                $users_data[$key]['ytb_video'] = $video_url;
+                break;
+            }
+        }
+        file_put_contents("Comptes/users.json", json_encode($users_data));
+
+        $_SESSION['ytb_video'] = $video_url;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -37,8 +61,13 @@
                 frameborder="0" allowfullscreen></allowfullscreen></iframe>
         <br></br>
         <!-- Input field to enter the new YouTube video URL -->
-        <input type="text" id="video-url-input" placeholder="Enter YouTube Video URL">
-        <button id="change-video-button">Change Video</button>
+        <!--<input type="text" id="video-url-input" placeholder="Enter YouTube Video URL">
+        <button id="change-video-button">Change Video</button>-->
+        
+        <form action="" method="POST">
+            <input type="text" id="video-url-input" name="video-url-input" placeholder="Enter YouTube Video URL">
+            <input type="submit" id="change-video-button" value="changer">
+        </form>
 
         <!-- Script to handle dynamic URL change -->
         <script>
@@ -59,14 +88,6 @@
                 if (videoId) {
                     iframe.src = 'https://www.youtube.com/embed/' + videoId;
                 }
-
-                document.getElementById('change-video-button').addEventListener('click', function() {
-                    var newVideoUrl = document.getElementById('video-url-input').value;
-                    var newVideoId = getVideoId(newVideoUrl);
-                    if (newVideoId) {
-                        iframe.src = 'https://www.youtube.com/embed/' + newVideoId;
-                    }
-                });
             });
         </script>
     </div>
