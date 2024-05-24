aebd459f40d71e145400719c1d50bc8f3cf2bbb2
Xeliok <130991034+Xeliok@users.noreply.github.com>
2024-05-24 22:51:49 +0200

    Delete Tests directory

--- Tests/script_search.js
+++
@@ -1,8 +0,0 @@
-const movingText = document.getElementById('moving-text');
-
-document.addEventListener('mousemove', (e) => {
-  const xPos = 10 -((e.clientX / window.innerWidth) * 7);
-  const yPos = 10 - ((e.clientY / window.innerHeight) * 7);
-  
-  movingText.style.transform = `translate(-${xPos}%, -${yPos}%)`;
-});
