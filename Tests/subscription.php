a1aa89a887996591673347d0bf57392cd9e04f9a
BA Louqman <130762713+TheLog7@users.noreply.github.com>
2024-05-26 03:37:15 +0200

    Delete Tests directory

--- Tests/subscription.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION['sub'] = 1;
-$json = json_decode(file_get_contents('./Comptes/'.$_SESSION['mail'].'.json'), true);
-$json['sub'] = 1;
-file_put_contents('./Comptes/'.$_SESSION['mail'].'.json', json_encode($json));
-header('Location:profil.php');
-?>
